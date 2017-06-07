3c5137306c0e2a0ed3c36f4d41cbfa7099e16cfb
rimfezai <rim.fezaii@gmail.com>
2017-06-07 10:49:28 +0100

    first change main.java

--- localRepositorySales/SalesTool/src/main/java/ca/demo/SalesTool/Main.java
+++ localRepositorySales/SalesTool/src/main/java/ca/demo/SalesTool/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	    
 	    private static void  displayGreeting()
 	    {
-	    	System.out.println("Hello happy sales dataa");
+	    	System.out.println("Hello happy sales dataa test2");
 	    	System.out.println("this app shows sales data");
 	    	System.out.println("test 1");
 	    }
